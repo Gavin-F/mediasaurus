a46adafff4c63249c5cf5c4927c63bcf43a11be5
Gavin-F <Gavinf96@hotmail.com>
2016-10-29 20:48:22 -0700

    movies page update

--- public/controller.js
+++ public/controller.js
@@ -174,6 +174,32 @@ index.controller("password-controller", function($scope,$location) {
 ///////////////////////////////////////////////////////
 index.controller("movie-controller", function($scope,$location) {
 
+    $('.rating').likeDislike({
+        initialValue: 0,
+        click: function (value, l, d, event) {
+            var likes = $(this.element).find('.likes');
+            var dislikes = $(this.element).find('.dislikes');
+
+            likes.text(parseInt(likes.text()) + l);
+            dislikes.text(parseInt(dislikes.text()) + d);
+
+            // $.ajax({
+            //     url: 'url',
+            //     type: 'post',
+            //     data: 'value=' + value,
+            // });
+        }
+    });
+
+    $(function () {
+    	$("#rateYo").rateYo({
+    		starWidth: "20px",
+    		numStars: 10,
+    		readOnly: true,
+    		rating: "88%"
+    	});
+    })
+
 });
 ///////////////////////////////////////////////////////
 // Account CONTROLLER
