e7b21cda666200788779aec06434c03dd9d9baa5
Derek Tam <derekkx@gmail.com>
2016-11-05 11:36:08 -0700

    Initial commit

--- models/models.js
+++ models/models.js
@@ -5,16 +5,26 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema({
     username: String,
     password: String, //hash created from password
-	email: String,
+	  email: String,
     //created_at: {type: Date, default: Date.now}
-	movieProfile: {type: Schema.ObjectId, ref: 'MovieProfile'}
+	  movieProfile: {type: Schema.ObjectId, ref: 'MovieProfile'},
+    suggestedMovies: [{movie: String}]
+    // suggestedMovies: {
+    //   type: [{movie: String}],
+    //   validate: [arrayLimit, '{PATH} exceeds the limit of 20']
+    // }
 });
+
+// // Validate limit http://stackoverflow.com/questions/28514790/how-to-set-limit-for-array-size-in-mongoose-schema/29418656#29418656
+// function arrayLimit(val){
+//   return val.length <= 20;
+// }
 
 var postSchema = new Schema({
     created_by: { type: Schema.ObjectId, ref: 'User' },
     created_at: {type: Date, default: Date.now},
     text: String
-}); 
+});
 
 var movieProfileSchema = new Schema({
 	genres: [{genre: String}],
@@ -22,7 +32,7 @@ var movieProfileSchema = new Schema({
 	directors: [{director: String}],
 	rating: Number,
 	length: Number,
-	
+
 	prefs: [{movie: String, liked: Boolean}]
 });
 
---
+++ routes/algorithms.js
@@ -0,0 +1,34 @@
+/*
+  Algorithm structure;
+
+  top level method:
+    - getSuggestedMovies
+      - Get a stored list of 20 movie ids from user profile
+      - calcSuggestedMovies if null
+    - calcSuggestedMovies
+      - Grab categories from user profile (genre, cast, rating)
+      - Weigh categories on point scale
+      - Use API to return relevant movies
+      - Return top 20 scoring movies
+
+    -
+*/
+
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var MovieProfile = mongoose.model('MovieProfile');
+var express = require('express');
+var tmdb_api = require('/api/tmdb_api')
+// require tmdb APIs
+
+function calc(user) {
+  var profile = user.movieProfile;
+  var newSuggestedMovies;
+  //discoverMovieswithGenre(); // Returns movies of preferred genre
+  // Sort list of rating
+  // Store list in suggestedMovies
+  user.update({ suggestedMovies: newSuggestedMovies}, function(err, numAffected){
+    if(err)return res.send(509,err);
+  })
+
+}
