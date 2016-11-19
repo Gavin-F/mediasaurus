8fc1cf61f39dbe9cae2a74330c9a77c6bcf8796c
Kevin Qiu <kevinqiu96@gmail.com>
2016-11-19 13:43:34 -0800

    removed # character from url

--- public/controller.js
+++ public/controller.js
@@ -12,7 +12,8 @@ var index = angular.module("index",
 	]);
 
 //Branch Comment
-index.config(function($routeProvider) {
+index.config(function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
 	$routeProvider
 	.when("/", {
 		templateUrl: "/html/home.html",
