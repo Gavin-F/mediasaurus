29c2ba2451ca79377f88a7ee38d28e44df7dd67a
Benjamin Kiang <benkiang@alumni.ubc.ca>
2016-12-02 22:18:32 -0800

    change email halfway done

--- public/controllers/email-controller.js
+++ public/controllers/email-controller.js
@@ -1,6 +1,16 @@
 angular.module("index.email", ["ngRoute"]).controller("email", function($scope, $http) {
 	$scope.$on("emailEvent", function(event, user) { // receive from child controller
 		$http.put("/auth/users/"+user.userID,user).success(function(req) { // authenticate user sign up
+			console.log(req);
+			if(req.error) { // indicates signup was successful
+				$scope.$broadcast("emailError", true);
+			}
+			else { // failed signup, show alert
+				$scope.$broadcast("emailUpdate");
+			}
+		})
+		.error(function(req){
+			console.log(req);
 			if(req.error) { // indicates signup was successful
 				$scope.$broadcast("emailError", true);
 			}
